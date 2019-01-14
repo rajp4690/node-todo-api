b7db189cbb42f5abf6997f3fb36136af63a5755e
Raj Patel <rajp4690@gmail.com>
2019-01-14 17:41:26 -0500

    Fix for MongoDB Connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
-var MONGODB_URI = 'mongodb://NodeAPITest:Node.API.Test123@ds257314.mlab.com:57314/nameless-spire-15101';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || MONGODB_URI,{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp',{ useNewUrlParser: true });
 
 module.exports = {mongoose};
