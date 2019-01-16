72fc29c12f4cbcea0d2601e2c52a1ab1a2880e3d
Raj Patel <rajp4690@gmail.com>
2019-01-16 16:46:33 -0500

    Add tests for GET /users/me and POST /users

--- server/config/config.js
+++ server/config/config.js
@@ -3,9 +3,9 @@ console.log('env ******', env);
 
 if(env === 'development') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
 }
 else if(env === 'test') {
     process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
 }
