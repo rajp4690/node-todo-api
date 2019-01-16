65a9cb7e66e07e477b3c3ff7e24a15fb1232b897
Raj Patel <rajp4690@gmail.com>
2019-01-15 20:08:07 -0500

    Add GET /users/me and authentication middleware

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,5 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true });
-mongoose.set('useCreateIndex', true);
 
 module.exports = {mongoose};
